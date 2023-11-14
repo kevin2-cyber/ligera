48200db88c4ade39bb50881391dc08ca7d247feb
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-11-14 01:08:44 +0000

    onboarding adapter

---
+++ mobile/app/src/main/java/com/ligera/app/model/Onboarding.java
@@ -0,0 +1,17 @@
+package com.ligera.app.model;
+
+public class Onboarding {
+    int image;
+
+    public Onboarding(int image) {
+        this.image = image;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
---
+++ mobile/app/src/main/java/com/ligera/app/view/adapter/OnboardingAdapter.java
@@ -0,0 +1,58 @@
+package com.ligera.app.view.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ligera.app.R;
+import com.ligera.app.model.Onboarding;
+
+import java.util.List;
+
+public class OnboardingAdapter extends RecyclerView.Adapter<OnboardingAdapter.OnboardingViewHolder> {
+
+    List<Onboarding> onboardings;
+    Context context;
+
+    public OnboardingAdapter(List<Onboarding> onboardings, Context context) {
+        this.onboardings = onboardings;
+        this.context = context;
+    }
+
+
+    @NonNull
+    @Override
+    public OnboardingViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.onboarding_item, parent, false);
+        return new OnboardingViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull OnboardingViewHolder holder, int position) {
+        holder.setOnboardingData(onboardings.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return onboardings.size();
+    }
+
+    static class OnboardingViewHolder extends RecyclerView.ViewHolder {
+        private ImageView imageView;
+
+        public OnboardingViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.iv_onboarding);
+        }
+        void setOnboardingData(Onboarding onboarding) {
+            imageView.setImageResource(onboarding.getImage());
+        }
+    }
+}
