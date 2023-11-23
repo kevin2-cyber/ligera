d3d4717014e4a5b72ba6c6085b63a8f73eb709ad
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-11-23 20:35:26 +0000

    created simple HomeActivity

---
+++ mobile/app/src/main/java/com/ligera/app/view/HomeActivity.java
@@ -0,0 +1,53 @@
+package com.ligera.app.view;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.ligera.app.R;
+import com.ligera.app.view.fragments.CartFragment;
+import com.ligera.app.view.fragments.FavoritesFragment;
+import com.ligera.app.view.fragments.HomeFragment;
+import com.ligera.app.view.fragments.ProfileFragment;
+
+public class HomeActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
+    HomeFragment homeFragment = new HomeFragment();
+    CartFragment cartFragment = new CartFragment();
+    FavoritesFragment favoritesFragment = new FavoritesFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.navigation_home);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.navigation_home) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,homeFragment).commit();
+            return true;
+        } else if (item.getItemId() == R.id.navigation_cart) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,cartFragment).commit();
+            return true;
+        } else if (item.getItemId() == R.id.navigation_liked) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,favoritesFragment).commit();
+            return true;
+        } else if (item.getItemId() == R.id.navigation_profile) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,profileFragment).commit();
+            return true;
+        }
+        return false;
+    }
+}
