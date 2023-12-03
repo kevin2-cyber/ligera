36aacb9d92d9d8836015f257be5a63b29c5fd2fa
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-12-03 20:56:35 +0000

    changed onCreateView

--- mobile/app/src/main/java/com/ligera/app/view/fragments/HomeFragment.java
+++ mobile/app/src/main/java/com/ligera/app/view/fragments/HomeFragment.java
@@ -2,19 +2,22 @@ package com.ligera.app.view.fragments;
 
 import android.os.Bundle;
 
-import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import com.ligera.app.R;
 
+import java.util.Objects;
+
 
 public class HomeFragment extends Fragment {
+//    private FragmentHomeBinding binding;
 
     SearchView searchView;
     TextView appTitle;
@@ -25,11 +28,21 @@ public class HomeFragment extends Fragment {
     }
 
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        disableViews();
+    }
+
+    private void disableViews() {
         searchView = requireActivity().findViewById(R.id.searchView);
         appTitle = requireActivity().findViewById(R.id.app_title);
         notification = requireActivity().findViewById(R.id.iv_notification);
@@ -42,10 +55,5 @@ public class HomeFragment extends Fragment {
         });
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
-    }
+
 }
