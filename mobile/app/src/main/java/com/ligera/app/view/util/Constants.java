ed99dea7ed097104a549c95312a9867a8ddaa915
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-12-06 17:21:39 +0000

    added selected drawables for bottom nav icons

--- mobile/app/src/main/java/com/ligera/app/view/util/Constants.java
+++ mobile/app/src/main/java/com/ligera/app/view/util/Constants.java
@@ -9,13 +9,13 @@ public class Constants {
     public static ArrayList<Product> getProductData() {
         ArrayList<Product> products = new ArrayList<>();
         products.add(new Product("Hoodie", R.drawable.hoodie, "Hello", "200", 2, 1, "Ligera","XL"));
-        products.add(new Product("Hoodie", R.drawable.hoodie, "Hello", "200", 2, 1, "Ligera","XL"));
-        products.add(new Product("Hoodie", R.drawable.hoodie, "Hello", "200", 2, 1, "Ligera","XL"));
-        products.add(new Product("Hoodie", R.drawable.hoodie, "Hello", "200", 2, 1, "Ligera","XL"));
-        products.add(new Product("Hoodie", R.drawable.hoodie, "Hello", "200", 2, 1, "Ligera","XL"));
-        products.add(new Product("Hoodie", R.drawable.hoodie, "Hello", "200", 2, 1, "Ligera","XL"));
-        products.add(new Product("Hoodie", R.drawable.hoodie, "Hello", "200", 2, 1, "Ligera","XL"));
-        products.add(new Product("Hoodie", R.drawable.hoodie, "Hello", "200", 2, 1, "Ligera","XL"));
+        products.add(new Product("Hoodie", R.drawable.hoodie, "Hello", "400", 2, 1, "Ligera","XL"));
+        products.add(new Product("Hoodie", R.drawable.hoodie, "Hello", "800", 2, 1, "Ligera","XL"));
+        products.add(new Product("Hoodie", R.drawable.hoodie, "Hello", "1000", 2, 1, "Ligera","XL"));
+        products.add(new Product("Hoodie", R.drawable.hoodie, "Hello", "1200", 2, 1, "Ligera","XL"));
+        products.add(new Product("Hoodie", R.drawable.hoodie, "Hello", "1400", 2, 1, "Ligera","XL"));
+        products.add(new Product("Hoodie", R.drawable.hoodie, "Hello", "1600", 2, 1, "Ligera","XL"));
+        products.add(new Product("Hoodie", R.drawable.hoodie, "Hello", "1800", 2, 1, "Ligera","XL"));
         return products;
     }
 }
