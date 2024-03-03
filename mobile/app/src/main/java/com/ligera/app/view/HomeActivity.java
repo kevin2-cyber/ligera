91cd2db780a70ab841ad41753cdb888a65d9261c
Kelvin Eduful <keduful@st.ug.edu.gh>
2024-03-03 13:06:18 +0000

    still not working

--- mobile/app/src/main/java/com/ligera/app/view/HomeActivity.java
+++ mobile/app/src/main/java/com/ligera/app/view/HomeActivity.java
@@ -49,9 +49,6 @@ public class HomeActivity extends AppCompatActivity implements NavigationBarView
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-//                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         EdgeToEdge.enable(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
