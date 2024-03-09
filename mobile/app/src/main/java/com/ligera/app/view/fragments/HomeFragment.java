4f29ba39b9e687deafa1cad19ad012c66b0fc7a8
Kelvin Eduful <keduful@st.ug.edu.gh>
2024-03-09 13:29:31 +0000

    refactoring UI

--- mobile/app/src/main/java/com/ligera/app/view/fragments/HomeFragment.java
+++ mobile/app/src/main/java/com/ligera/app/view/fragments/HomeFragment.java
@@ -2,38 +2,14 @@ package com.ligera.app.view.fragments;
 
 import android.app.SearchManager;
 import android.app.SearchableInfo;
-import android.content.BroadcastReceiver;
 import android.content.ComponentName;
-import android.content.ContentResolver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.IntentSender;
-import android.content.ServiceConnection;
-import android.content.SharedPreferences;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.content.res.AssetManager;
-import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.database.DatabaseErrorHandler;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.UserHandle;
-import android.view.Display;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
-import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -44,26 +20,18 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.appbar.MaterialToolbar;
 import com.ligera.app.R;
 import com.ligera.app.databinding.FragmentHomeBinding;
 import com.ligera.app.model.entity.Product;
 import com.ligera.app.view.adapter.HomeRecyclerVA;
 import com.ligera.app.view.util.Constants;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class HomeFragment extends Fragment {
     private FragmentHomeBinding binding;
-    Toolbar toolbar;
     SearchView searchView;
     ArrayList<Product> products;
     HomeRecyclerVA adapter;
@@ -88,23 +56,6 @@ public class HomeFragment extends Fragment {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
     }
-//    @Override
-//    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-//        inflater.inflate(R.menu.app_bar_menu, menu);
-//
-//        MenuItem itemSearch = menu.findItem(R.id.search);
-//        // get the searchView and searchable configuration
-//        SearchManager searchManager = (SearchManager) requireActivity().getSystemService(Context.SEARCH_SERVICE);
-//        searchView = (SearchView) itemSearch.getActionView();
-//        searchView.setQueryHint("Type Here");
-//        ComponentName componentName = requireActivity().getComponentName();
-//        SearchableInfo searchableInfo = searchManager.getSearchableInfo(componentName);
-//        searchView.setSearchableInfo(searchableInfo);
-//        searchView.setIconified(false);
-//
-//
-//        super.onCreateOptionsMenu(menu, inflater);
-//    }
 
     @Nullable
     @Override
@@ -122,6 +73,7 @@ public class HomeFragment extends Fragment {
         // get the searchView and searchable configuration
         SearchManager searchManager = (SearchManager) requireActivity().getSystemService(Context.SEARCH_SERVICE);
         searchView = (SearchView) itemSearch.getActionView();
+        assert searchView != null;
         searchView.setQueryHint("Type Here");
         ComponentName componentName = requireActivity().getComponentName();
         SearchableInfo searchableInfo = searchManager.getSearchableInfo(componentName);
@@ -145,7 +97,7 @@ public class HomeFragment extends Fragment {
         // get the list of products
         products = Constants.getProductData();
         // assign list of products to adapter
-        adapter = new HomeRecyclerVA(getActivity(), products);
+        adapter = new HomeRecyclerVA(getActivity());
         RecyclerView recyclerView = binding.rvItems;
         recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
