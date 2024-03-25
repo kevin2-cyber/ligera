36fdb4bcf5e70c716940f0d7a07677394d3072a5
Kelvin Eduful <keduful@st.ug.edu.gh>
2024-03-25 14:30:31 +0000

    changing app theme

--- mobile/app/src/main/java/com/ligera/app/view/fragments/FavoritesFragment.java
+++ mobile/app/src/main/java/com/ligera/app/view/fragments/FavoritesFragment.java
@@ -1,67 +1,78 @@
 package com.ligera.app.view.fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import com.ligera.app.R;
+import com.ligera.app.databinding.FragmentFavoritesBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FavoritesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class FavoritesFragment extends Fragment {
+    private FragmentFavoritesBinding binding;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String SWITCH_BUTTON_KEY = "switch";
+    private static final String PREF_KEY = "pref";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
 
     public FavoritesFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FavoritesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FavoritesFragment newInstance(String param1, String param2) {
-        FavoritesFragment fragment = new FavoritesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        SharedPreferences sharedPreferences = view.getContext().getSharedPreferences(PREF_KEY, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        updateUI(sharedPreferences);
+
+        binding.switchButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                editor.putBoolean(SWITCH_BUTTON_KEY, true).apply();
+                updateUI(sharedPreferences);
+            } else {
+                editor.putBoolean(SWITCH_BUTTON_KEY, false).apply();
+                updateUI(sharedPreferences);
+            }
+        });
+    }
+
+    private void updateUI(SharedPreferences sharedPreferences) {
+        boolean isChecked = sharedPreferences.getBoolean(SWITCH_BUTTON_KEY, false);
+        binding.switchButton.setChecked(isChecked);
+        if (isChecked) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorites, container, false);
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_favorites, container, false);
+        return binding.getRoot();
     }
 }
