c78ffe508c1fe4b9fdcb1e922be31b72dece063a
Kevin2-cyber <kieduful47@gmail.com>
2025-05-23 15:04:21 +0000

    cleaning up the onboarding screen

--- mobile/app/src/main/java/com/ligera/app/view/OnboardingActivity.java
+++ mobile/app/src/main/java/com/ligera/app/view/OnboardingActivity.java
@@ -4,33 +4,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.transition.Explode;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.ligera.app.R;
 import com.ligera.app.model.entity.Onboarding;
 import com.ligera.app.view.adapter.OnboardingAdapter;
 import com.ligera.app.view.util.DepthPageTransformer;
+import com.tbuonomo.viewpagerdotsindicator.DotsIndicator;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import ru.tinkoff.scrollingpagerindicator.ScrollingPagerIndicator;
 
 public class OnboardingActivity extends AppCompatActivity {
 
     ViewPager2 onboardingViewPager;
     OnboardingAdapter onboardingAdapter;
     Button shopNowBtn;
-//    LinearLayout onboardingIndicators;
 
     List<Onboarding> onboardings;
 
@@ -45,7 +39,6 @@ public class OnboardingActivity extends AppCompatActivity {
 
 
         shopNowBtn = findViewById(R.id.shop_now_btn);
-//        onboardingIndicators = findViewById(R.id.onboarding_indicators);
 
         setupOnboardingItems();
 
@@ -54,8 +47,8 @@ public class OnboardingActivity extends AppCompatActivity {
         onboardingViewPager.setAdapter(onboardingAdapter);
         onboardingViewPager.setPageTransformer(new DepthPageTransformer());
 
-        ScrollingPagerIndicator scaleDotsIndicator = findViewById(R.id.indicator);
-        scaleDotsIndicator.attachToPager(onboardingViewPager);
+        DotsIndicator scaleDotsIndicator = findViewById(R.id.indicator);
+        scaleDotsIndicator.attachTo(onboardingViewPager);
 
         onboardingViewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             /**
@@ -106,51 +99,4 @@ public class OnboardingActivity extends AppCompatActivity {
 
         onboardingAdapter = new OnboardingAdapter(onboardings, this);
     }
-
-//    private void setupOnboardingIndicator() {
-//        ImageView[] indicators = new ImageView[onboardingAdapter.getItemCount()];
-//        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-//                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
-//        );
-//        params.setMargins(8,0,8,0);
-//        for (int i = 0; i < indicators.length; i++) {
-//            indicators[i] = new ImageView(this);
-//            indicators[i].setImageDrawable(
-//                    ContextCompat.getDrawable(
-//                            getApplicationContext(),
-//                            R.drawable.default_dot
-//                    )
-//            );
-//            indicators[i].setLayoutParams(params);
-//            onboardingIndicators.addView(indicators[i]);
-//        }
-//    }
-
-//    private void setCurrentOnboardingIndicator(int index) {
-//        int childCount = onboardingIndicators.getChildCount();
-//        for (int i = 0; i < childCount; i++) {
-//            ImageView imageView = (ImageView) onboardingIndicators.getChildAt(i);
-//            if (i == index) {
-//                imageView.setImageDrawable(
-//                        ContextCompat.getDrawable(
-//                                getApplicationContext(),
-//                                R.drawable.selected_dot
-//                        )
-//                );
-//            } else {
-//                imageView.setImageDrawable(
-//                        ContextCompat.getDrawable(
-//                                getApplicationContext(),
-//                                R.drawable.default_dot
-//                        )
-//                );
-//            }
-//        }
-//
-//        if (index == onboardingAdapter.getItemCount() - 1) {
-//            shopNowBtn.setVisibility(View.VISIBLE);
-//        } else {
-//            shopNowBtn.setVisibility(View.GONE);
-//        }
-//    }
 }
